b65ff52045c213495a7913bee50d14da1a8f37ba
Borys Staszczak <35380525+BlueCookieFrog@users.noreply.github.com>
2021-05-12 11:05:05 +0200

    sql

---
+++ sql/drop.sql
@@ -0,0 +1,16 @@
+use BD;
+
+-- drop table assigned_location;
+-- drop table assigned_machine;
+-- drop table locations;
+-- drop table location_type;
+-- drop table machines;
+-- drop table machine_status;
+-- drop table machine_type;
+-- drop table crashReport;
+-- drop table personalData;
+-- drop table report_type;
+-- drop table URworker;
+-- drop table worker;
+-- drop table worker_position;
+
