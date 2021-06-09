07e9e8e7042ea632ec06895a55424939120e11c6
Borys Staszczak <35380525+BlueCookieFrog@users.noreply.github.com>
2021-06-09 09:13:11 +0200

    ehhhhhh

--- based_dane/based_dane/settings.py
+++ based_dane/based_dane/settings.py
@@ -133,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
+
+LOGIN_REDIRECT_URL="/home"
--- based_dane/main/forms.py
+++ based_dane/main/forms.py
@@ -1,12 +1,29 @@
+import datetime
 from django import forms
-from .models import Machines
+from .models import Machines, WorkerPosition, Personaldata
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 # from fieldmaker.form import ExpandableFrom, ExpandableModelForm
 
 
 def machines():
     choices = []
     for each in Machines.objects.all():
-        choices.append((str(each.id_machine), each.name))
+        choices.append((each.id_machine, each.name))
+
+    return choices
+
+def worker_position():
+    choices = []
+    for each in WorkerPosition.objects.all():
+        choices.append((each.id_worker_position, each.worker_position_type))
+
+    return choices
+
+def workers():
+    choices = []
+    for each in Personaldata.objects.all():
+        choices.append((each.id, each.name+" "+each.surname))
 
     return choices
 
@@ -17,8 +34,29 @@ type_choices = (
     ("Inny", "Inny")
     )
 
+lic_choices = (
+    ("Office 360", "Office 360"),
+    ("Norton", "Norton"),
+    ("Jira", "Jira")
+)
+
 class Report(forms.Form):
 
+    reporting_worker = forms.ChoiceField(choices = workers(), label="Pracownik zgłaszający")
+    report_date = forms.DateTimeField(initial=datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     machine = forms.ChoiceField(choices = machines(), label = "Maszyna ",)
     damage_type = forms.ChoiceField(choices = type_choices, label= "Typ Awarii")
     description = forms.CharField(max_length=255, label="Opis problemu")
+
+class SignUpWorker(forms.Form):
+    name = forms.CharField(max_length=255, help_text="Wymagane.")
+    surname = forms.CharField(max_length=255, help_text="Wymagane.")
+    birthday = forms.DateField(initial=datetime.datetime.now().strftime("%Y-%m-%d"))
+    gender = forms.ChoiceField(choices= (( "female", "Kobieta"),("male", "Mężczyzna")))
+    position = forms.ChoiceField(choices=worker_position(), help_text="Wymagane.")
+    licenses = forms.MultipleChoiceField(choices=lic_choices)
+
+
+class ResolveReport(forms.Form):
+
+    resolve_date = forms.DateTimeField(initial=datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
